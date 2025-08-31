8d14455efef934f48b25a3219b879b138f647923
DSchacht0825 <danielschacht0825@gmail.com>
2025-08-30 22:48:48 -0700

    Fix unused imports in Home.tsx to clean up build warnings

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { useScrollAnimation } from '../hooks/useScrollAnimation';
 
 const Home: React.FC = () => {
-  const overviewRef = useScrollAnimation('animate-fade-up', { once: true });
 
   return (
     <main 
